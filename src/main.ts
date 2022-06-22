4221473ce2601ff4dd2c263f8268e70538ae67c5
Lucas-Gardini <lucasgardini.dias@hotmail.com>
2022-06-22 18:46:47 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "primeicons/primeicons.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+
+createApp(App).use(PrimeVue).mount("#app");
